e3d5a2df601037b887a56847ab9165744bd20e99
nemdevcode <nemdevcode@gmail.com>
2025-04-02 19:25:41 +0200

    Subir avances a gitHub

--- models/gestores_model.py
+++ models/gestores_model.py
@@ -25,18 +25,18 @@ class GestoresCollection:
         self.fecha_inactivacion = fecha_inactivacion
         self.estado_gestor = estado_gestor
 
-class UsuariosGestoresCollection:
-    def __init__(self,
-                 usuario_rol_id,
-                 gestor_id,
-                 fecha_activacion,
-                 fecha_modificacion,
-                 fecha_inactivacion,
-                 estado_usuario_gestor):
-        self.usuario_rol_id = usuario_rol_id
-        self.gestor_id = gestor_id
-        self.fecha_activacion = fecha_activacion
-        self.fecha_modificacion = fecha_modificacion
-        self.fecha_inactivacion = fecha_inactivacion
-        self.estado_usuario_gestor = estado_usuario_gestor
+# class UsuariosGestoresCollection:
+#     def __init__(self,
+#                  usuario_rol_id,
+#                  gestor_id,
+#                  fecha_activacion,
+#                  fecha_modificacion,
+#                  fecha_inactivacion,
+#                  estado_usuario_gestor):
+#         self.usuario_rol_id = usuario_rol_id
+#         self.gestor_id = gestor_id
+#         self.fecha_activacion = fecha_activacion
+#         self.fecha_modificacion = fecha_modificacion
+#         self.fecha_inactivacion = fecha_inactivacion
+#         self.estado_usuario_gestor = estado_usuario_gestor
 
