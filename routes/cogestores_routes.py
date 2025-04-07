802b7f5db490fb2c9ed4f722627f6ec80c85d646
nemdevcode <nemdevcode@gmail.com>
2025-04-07 12:48:53 +0200

    Subir avances a gitHub

--- routes/cogestores_routes.py
+++ routes/cogestores_routes.py
@@ -11,12 +11,12 @@ from core._decoradores import login_requerido
 cogestores_bp = Blueprint('cogestores', __name__)
 
 # Rutas para usuarios cogestores
-@cogestores_bp.route('/usuarios/cogestores', methods=['GET'])
+@cogestores_bp.route('/usuarios/usuarios-cogestores', methods=['GET', 'POST'])
 @login_requerido
 def usuarios_cogestores():
     return usuarios_cogestores_vista()
 
-@cogestores_bp.route('/usuarios/cogestores/gestor', methods=['GET'])
+@cogestores_bp.route('/usuarios/usuarios-cogestores/gestor', methods=['GET', 'POST'])
 @login_requerido
 def usuarios_cogestores_gestor():
     return usuarios_cogestores_gestor_vista() 
