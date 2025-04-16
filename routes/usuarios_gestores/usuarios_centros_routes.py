0846addc82b187e60b2fef0fcb63bcc81c55e20f
nemdevcode <nemdevcode@gmail.com>
2025-04-16 19:23:42 +0200

    Subir avances a gitHub

--- routes/usuarios_gestores/usuarios_centros_routes.py
+++ routes/usuarios_gestores/usuarios_centros_routes.py
@@ -19,25 +19,25 @@ Rutas para gestión de usuarios centros, funciones:
     - usuarios_centros_eliminar_vista()
 '''
 
-@ug_usuarios_centros_bp.route('/', methods=['GET', 'POST'])
+@ug_usuarios_centros_bp.route('/<gestor_id>/<titular_id>/<centro_id>', methods=['GET', 'POST'])
 @login_requerido
-def usuarios_centros():
-    return usuarios_centros_vista()
+def usuarios_centros(gestor_id, titular_id, centro_id):
+    return usuarios_centros_vista(gestor_id, titular_id, centro_id)
 
-@ug_usuarios_centros_bp.route('/crear', methods=['GET', 'POST'])
+@ug_usuarios_centros_bp.route('/crear/<gestor_id>/<titular_id>/<centro_id>', methods=['GET', 'POST'])
 @login_requerido
-def usuarios_centros_crear():
-    return usuarios_centros_crear_vista()
+def usuarios_centros_crear(gestor_id, titular_id, centro_id):
+    return usuarios_centros_crear_vista(gestor_id, titular_id, centro_id)
 
-@ug_usuarios_centros_bp.route('/actualizar', methods=['GET', 'POST'])
+@ug_usuarios_centros_bp.route('/actualizar/<gestor_id>/<titular_id>/<centro_id>', methods=['GET', 'POST'])
 @login_requerido
-def usuarios_centros_actualizar():
-    return usuarios_centros_actualizar_vista()
+def usuarios_centros_actualizar(gestor_id, titular_id, centro_id):
+    return usuarios_centros_actualizar_vista(gestor_id, titular_id, centro_id)
 
-@ug_usuarios_centros_bp.route('/eliminar', methods=['GET', 'POST'])
+@ug_usuarios_centros_bp.route('/eliminar/<gestor_id>/<titular_id>/<centro_id>', methods=['GET', 'POST'])
 @login_requerido
-def usuarios_centros_eliminar():
-    return usuarios_centros_eliminar_vista()
+def usuarios_centros_eliminar(gestor_id, titular_id, centro_id):
+    return usuarios_centros_eliminar_vista(gestor_id, titular_id, centro_id)
 
 
 
